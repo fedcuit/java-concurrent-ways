0f583eff0eb04b695a085dff42780098f5fbfa48
Erdong Feng <edfeng@thoughtworks.com>
2015-07-08 17:50:58 +0800

    explain the purpose of DataProcessorBasedOnStream class.

--- src/main/java/io/github/fedcuit/concurrent/dataprocess/DataProcessorBasedOnStream.java
+++ src/main/java/io/github/fedcuit/concurrent/dataprocess/DataProcessorBasedOnStream.java
@@ -2,6 +2,10 @@ package io.github.fedcuit.concurrent.dataprocess;
 
 import static java.util.stream.LongStream.range;
 
+/**
+ * This class is used to demonstrate that parallel is much faster when deal with large data set and each computation unit
+ * is time consuming.
+ */
 public class DataProcessorBasedOnStream {
     public static long timeConsumingOperationOnLargeData() {
         return range(0, 1_000).map(VerySlowService::timeConsumingOperation).sum();
